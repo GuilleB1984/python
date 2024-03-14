db3d3a6a212fdbafd1ee7f0d289ca7c80b7fedfc
GuilleB1984 <131191415+GuilleB1984@users.noreply.github.com>
2024-03-14 11:58:30 -0300

    Create Ejercicio1.py

---
+++ Ejercicio1.py
@@ -0,0 +1,9 @@
+# Pedir al usuario que ingrese dos números
+numero1 = int(input("Ingrese el primer número: "))
+numero2 = int(input("Ingrese el segundo número: "))
+
+# Mostrar la suma, resta, multiplicación y división de esos números
+print("Suma:", numero1 + numero2)
+print("Resta:", numero1 - numero2)
+print("Multiplicación:", numero1 * numero2)
+print("División:", numero1 / numero2)
